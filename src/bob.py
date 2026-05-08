57d55f0fd814d82501e598329947f9305fd1d797
Adeel <adeel.hassan2k20@gmail.com>
2026-05-08 11:47:11 +0500

    bob work done -> qubit to secret key

---
+++ src/bob.py
@@ -0,0 +1,53 @@
+# Bob: Receiver -> chooses bases, measures qubits
+
+import random
+from qiskit import Aer, execute
+
+class Bob:
+    def __init__(self):
+        self.bases = []
+        self.results = []
+    
+    def choose_bases(self, n):
+        """Bob randomly chooses measurement bases"""
+        self.bases = [random.choice(['+', 'x']) for _ in range(n)]
+        return self.bases
+    
+    def measure_qubit(self, circuit, basis):
+        """
+        Measure qubit in given basis.
+        For x-basis, apply H gate before measurement.
+        """
+        qc = circuit.copy()
+        
+        # Transform basis if measuring in x
+        if basis == 'x':
+            qc.h(0)
+        
+        # Measure
+        qc.measure(0, 0)
+        
+        # Run simulation
+        simulator = Aer.get_backend('qasm_simulator')
+        job = execute(qc, simulator, shots=1)
+        result = job.result()
+        counts = result.get_counts()
+        
+        return int(list(counts.keys())[0])
+    
+    def measure_all(self, circuits):
+        """Measure all received qubits"""
+        self.results = []
+        
+        for circuit, basis in zip(circuits, self.bases):
+            result = self.measure_qubit(circuit, basis)
+            self.results.append(result)
+        
+        return self.results
+    
+    def get_data(self):
+        """Return all Bob's data"""
+        return {
+            'bases': self.bases,
+            'results': self.results
+        }
